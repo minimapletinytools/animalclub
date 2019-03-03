ea56e2c88355f06d676b9ef6511d728936f12149
Peter Lu <chippermonky@gmail.com>
2019-03-03 15:35:14 -0800

    added pictures to readme

--- src/AnimalClub/ForeignBindings.hs
+++ src/AnimalClub/ForeignBindings.hs
@@ -12,10 +12,27 @@ TODO
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module AnimalClub.ForeignBindings (
+  breed_hs
 ) where
 
+import Foreign
+import Foreign.C.Types
+
+breed_hs ::
+  CInt -- ^ random seed
+  -> Ptr CChar -- ^ DNA bytes 1
+  -> Ptr CChar -- ^ DNA bytes 2
+  -> Ptr CChar -- ^ DNA output
+  -> CSize -- ^ DNA size
+  -> () -- ^ 😱
+breed_hs seed dna1 dna2 outdna size = undefined
 
 
+-- TODO figure out how to create genome 
+{- dnaProps_hs ::
+  Ptr CChar -- ^ DNA bytes
+  -> CSize -- ^ DNA size
+  -> Genome
+  ->
 
---import Foreign.C.String
---import Foreign.C.Types
+  -}
