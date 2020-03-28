a11b6ceaacae4fb64388d7b737c8b0f7eb9ea60d
pddla <chippermonky@gmail.com>
2020-03-27 21:10:42 -0700

    made AnimalExp parameterizable

--- src/AnimalClub/Animals/Animal.hs
+++ src/AnimalClub/Animals/Animal.hs
@@ -47,7 +47,7 @@ mans ::
  -> AnimalNode a
 mans n p t c = manf n [] p t c
 
--- | helper method for making AnimalNode
+-- | helper method for making AnimalNode with BoneFlags
 manf ::
  (AnimalFloat a)
  => T.Text -- ^ name
@@ -58,7 +58,7 @@ manf ::
  -> AnimalNode a
 manf n f p t c = manbt n f Same p t c
 
--- | helper method for making AnimalNode
+-- | helper method for making AnimalNode with BoneFlags and BoneTrans
 manbt ::
  (AnimalFloat a)
  => T.Text -- ^ name
@@ -72,29 +72,30 @@ manbt n f bt p t c = AnimalNode (BoneId n f) bt p t False c
 
 
 -- TODO add something like ExpSkellyFuncPrioritized
--- FUTURE is it possible to parmeterize AnimalExp to allow users to pass in their own data types
--- e.g. 'ExpUser T.Text a' or something like that... (ideally a list of types...)
 -- | various output expression of an animal genotype
-data AnimalExp a =
+data AnimalExp a b =
+  -- built in data type for bytestrings
   ExpBytes T.Text B.ByteString
-  | ExpFloats T.Text [a]
-  | ExpSkellyFunc (SkellyFunc a) deriving (Show)
+  -- built in data type to pass into skellygen
+  | ExpSkellyFunc (SkellyFunc a)
+  -- user defined data type
+  | ExpUser T.Text b deriving (Show)
 
 -- | pull out just the SkellyFuncs
-parseSkellyFuncs :: [AnimalExp a] -> [SkellyFunc a]
+parseSkellyFuncs :: [AnimalExp a b] -> [SkellyFunc a]
 parseSkellyFuncs = catMaybes . fmap (\case
   ExpSkellyFunc sf -> Just sf
   _ -> Nothing)
 
 -- | helper monad for writing out SkellyFuncs
-tellSkellyFunc :: (Monad m) => SkellyFunc a -> GenotypeT g [AnimalExp a] m ()
+tellSkellyFunc :: (Monad m) => SkellyFunc a -> GenotypeT g [AnimalExp a b] m ()
 tellSkellyFunc sf = tell [ExpSkellyFunc sf]
 
 -- | generates 'AnimalPropertyMap' from list of 'BoneId's and 'AnimalExp's
 generateAnimalProperties ::
   (AnimalFloat a)
   => [BoneId] -- ^ list of all bones
-  -> [AnimalExp a] -- ^ list of properties
+  -> [AnimalExp a b] -- ^ list of properties
   -> AnimalPropertyMap a-- ^ output accumulated map of properties.
 generateAnimalProperties bids = generateAnimalProperties_ bids . map (\sf -> PrioritizedSkellyFunc (0,sf)) . parseSkellyFuncs
 
